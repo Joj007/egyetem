36e04d45e07ee55da7564675fb06413aaae43b0f
Joj007 <71085151+Joj007@users.noreply.github.com>
2023-12-01 11:05:10 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bemenet = System.Console.ReadLine();
+            List<string> list = new List<string>();
+            list = bemenet.Split().ToList();
+            string eredmeny = "";
+            foreach (string item in list)
+            {
+                switch (item.Substring(item.Length-2))
+                {
+                    case "sp":
+                        for (int i = 1; i < 1 + Convert.ToInt32(item.Substring(0, item.Length - 2)); i++)
+                            eredmeny += " ";
+                        break;
+                    case "bS":
+                        for (int i = 1; i < 1 + Convert.ToInt32(item.Substring(0,item.Length-2)); i++)
+                            eredmeny+="\\";
+                        break;
+                    case "sQ":
+                        for (int i = 1; i < 1 + Convert.ToInt32(item.Substring(0, item.Length - 2)); i++)
+                            eredmeny+="\'";
+                        break;
+                    case "nl":
+                        eredmeny+="\n";
+                        break;
+                    default:
+                        for (int i = 1; i < 1 + Convert.ToInt32(item.Substring(0, item.Length-1)); i++) {
+                            eredmeny+=item.Substring(item.Length-1);
+                        }
+                        break;
+                }
+            }
+            System.Console.WriteLine(eredmeny);
+        }
+    }
+}
